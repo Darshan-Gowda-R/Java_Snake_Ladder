b058ea8d1b011b419eb65e9525e5ccc5fc07409e
darshan <darshan.gowda.r.100@gmail.com>
2022-01-25 09:33:55 +0530

    The Player then checks for a Option. They are No Play, Ladder or Snake .

---
+++ Snake_and_Ladder/SnakeUC2.java
@@ -0,0 +1,12 @@
+public class SnakeUC2{
+
+	public static void main(String args[]){
+
+	int position=0,dice=0;
+	dice=(int)Math.floor(Math.random()*10)%6+1;
+	System.out.println("player at position : "+position);
+	System.out.println("dice rolled : "+dice);
+
+	}
+
+}
